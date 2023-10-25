bcf6f194d296c58998eaf9e9afb46ad81a8e284f
M Wajdan Ismail <wajdan.mohammad@gmail.com>
2023-10-25 22:33:43 +0500

    navbar setup

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./styles/navbar.css";
+
+export default function Navbar() {
+  return (
+    <div className="nav">
+      <div className="navLeft">
+        <i class="nav-icons ri-github-fill"></i>
+        <i class="nav-icons ri-linkedin-box-fill"></i>
+        <i class="nav-icons ri-global-line"></i>
+      </div>
+      <div className="navCenter">
+        <ul className="navCenterlist">
+          <li className="navListItem">Home</li>
+          <li className="navListItem">About</li>
+          <li className="navListItem">Contact</li>
+          <li className="navListItem">Write</li>
+          <li className="navListItem">Logout</li>
+        </ul>
+      </div>
+      <div className="navRight">
+        <img
+          className="navImg"
+          src="https://images.pexels.com/photos/5076520/pexels-photo-5076520.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <i class="nav-icons ri-search-line"></i>
+      </div>
+    </div>
+  );
+}
